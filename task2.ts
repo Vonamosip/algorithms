325fa91e8342f55efb654a2b8cb3c57d420dc97d
Vonamosip <sashadrozdov94@gmail.com>
2025-05-05 15:01:13 +0300

    clear unnecessary files

--- task2.ts
+++ task2.ts
@@ -6,7 +6,7 @@
 // console.log(arrayDiff([1, 3, 3, 4], [1, 3, '4'])); -> [4, '4']
 
 function arrayDiff(arr1: any[], arr2: any[]): any {
-    const result = [];
+    const result:any = [];
     for (let i = 0; i < arr1.length; i++) {
         if (!arr2.includes(arr1[i])) {
             result.push(arr1[i]);
