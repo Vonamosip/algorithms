ff8973d92011e371077824e3cb87cc1955aa42cc
Vonamosip <sashadrozdov94@gmail.com>
2025-05-05 15:12:40 +0300

    task7

---
+++ task7.ts
@@ -0,0 +1,23 @@
+// Функция принимает массив meetups,
+//     и возвращает суммарное количество человек, находящихся на активных митапах
+
+// membersOnActiveMeetups(meetups); // 1500
+
+// Пример:
+//     const meetups = [
+//         { name: 'JavaScript', isActive: true, members: 100 },
+//         { name: 'Angular', isActive: true, members: 900 },
+//         { name: 'Node', isActive: false, members: 600 },
+//         { name: 'React', isActive: true, members: 500 },
+//     ];
+// membersOnActiveMeetups(meetups); // 1500
+
+function membersOnActiveMeetups(meetups:any[]): number {
+    let result = 0;
+    for (let i = 0; i < meetups.length; i++) {
+        if (meetups[i].isActive) {
+            result += meetups[i].members;
+        }
+    } 
+    return result;
+}
